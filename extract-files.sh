fa5676d9df8accbdd578dd542a79fa3fa52c4e5a
Christian Oder <myself5@carbonrom.org>
2022-01-25 22:42:56 +0100

    hotdogg: Patch fingerprint blobs to different prop
    libgf_ud_hal.so looks for vendor.boot.verifiedbootstate to be set to the phones true state
    in our case, for an unlocked phone, it must be orange
    (green = locked with factory rom, orange = unlocked no checking, yellow = locked with user key)
    
    Change it to look for vendor.boot.fingerprintbstate which we set to orange without having to mess with anything safetynet related.
    
    DarkJoker360: Switch to blob_fixup() and use vendor.prop
    
    Co-authored-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I34bfdda59878715c049140293f3662ae4b206562
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -18,4 +18,12 @@ export DEVICE=hotdogg
 export DEVICE_COMMON=sm8150-common
 export VENDOR=oneplus
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/libgf_ud_hal.so|vendor/lib64/libgf_ud_hal.so)
+            sed -i "s|vendor.boot.verifiedbootstate|vendor.boot.fingerprintbstate|g" "${2}"
+            ;;
+    esac
+}
+
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
