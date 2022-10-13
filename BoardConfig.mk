43b858db4f4c2660d0d60b9120c679a4e9343410
Bruno Martins <bgcngm@gmail.com>
2022-10-13 19:21:42 +0200

    hotdogg: Commonize bdroid_buildcfg
    Change-Id: I7b06908aa0f1c1012f77eb06049957bdcd6fcdff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,9 +46,6 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
-
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
